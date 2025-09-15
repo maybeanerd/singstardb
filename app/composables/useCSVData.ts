40fc97984aeb9e8e3b177ad85c85ac7c185e9c63
Sebastian Di Luzio <sebidiluzio@gmail.com>
2025-09-15 20:43:55 +0200

    wip

---
+++ app/composables/useCSVData.ts
@@ -0,0 +1,17 @@
+export function useCSVData() {
+
+
+    return {
+        data: ref([{
+            id: '4597',
+            date: '2024-03-10T19:45:00',
+            status: 'paid',
+            amount: 529
+        }, {
+            id: '4596',
+            date: '2024-03-10T15:55:00',
+            status: 'paid',
+            amount: 639
+        }])
+    }
+}
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+const { data } = useCSVData()
+</script>
+
 <template>
   <div class="flex flex-col items-center justify-center gap-4 h-screen">
     <h1 class="font-bold text-2xl text-(--ui-primary)">
@@ -5,21 +9,7 @@
     </h1>
 
     <div class="flex items-center gap-2">
-      <UButton
-        label="Documentation"
-        icon="i-lucide-square-play"
-        to="https://ui.nuxt.com/getting-started/installation/nuxt"
-        target="_blank"
-      />
-
-      <UButton
-        label="GitHub"
-        color="neutral"
-        variant="outline"
-        icon="i-simple-icons-github"
-        to="https://github.com/nuxt/ui"
-        target="_blank"
-      />
+      <UTable :data="data" class="flex-1" />
     </div>
   </div>
 </template>
